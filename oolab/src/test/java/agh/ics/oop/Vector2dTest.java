e127921c313619185620bdb491280ad7284b9193
pderbisz <115102223+pderbisz@users.noreply.github.com>
2022-10-19 14:36:55 +0200

    eee

--- oolab/src/test/java/agh/ics/oop/Vector2dTest.java
+++ oolab/src/test/java/agh/ics/oop/Vector2dTest.java
@@ -47,11 +47,25 @@ class Vector2dTest {
         Vector2d test = new Vector2d(1, 1);
         assertEquals(c, test);
     }
+    @Test void lowerLeftTest2(){
+        Vector2d a = new Vector2d(-10,-5);
+        Vector2d b = new Vector2d(-20,-3);
+        Vector2d c = a.lowerLeft(b);
+        Vector2d test = new Vector2d(-20, -5);
+        assertEquals(c, test);
+    }
     @Test void addVectorTest1(){
         Vector2d a = new Vector2d(1,3);
         Vector2d b = new Vector2d(2,-1);
         Vector2d c = a.add(b);
         Vector2d test = new Vector2d(3, 2);
+        assertEquals(c, test);
+    }
+    @Test void addVectorTest2(){
+        Vector2d a = new Vector2d(-10,-10);
+        Vector2d b = new Vector2d(10,10);
+        Vector2d c = a.add(b);
+        Vector2d test = new Vector2d(0, 0);
         assertEquals(c, test);
     }
     @Test void subtractVectorTest1(){
@@ -60,6 +74,13 @@ class Vector2dTest {
         Vector2d c = a.subtract(b);
         Vector2d test = new Vector2d(-1, 4);
         assertEquals(c, test);
+    }
+    @Test void subtractVectorTest2(){
+        Vector2d a = new Vector2d(0,0);
+        Vector2d b = new Vector2d(2,-1);
+        Vector2d c = a.subtract(b);
+        Vector2d test = new Vector2d(-1, 4);
+        assertFalse(c.equals(test));
     }
     @Test void oppositeTest1(){
         Vector2d a = new Vector2d(-1,3);
